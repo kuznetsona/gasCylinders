81ee79a8bf6b4e10821e07c23d5b9d6d84d3259d
Дарья <kud4v@yandex.ru>
2023-10-11 20:28:24 +0300

    c2 webcam

--- webcam_ocr.py
+++ webcam_ocr.py
@@ -60,30 +60,45 @@ class TesseractOCR:
         self.config = config
 
     def image_to_string(self, image_path):
-        return pytesseract.image_to_string(image_path, config=self.config, lang="rus")
+        return pytesseract.image_to_string(image_path, config=self.config)
 
-if __name__ == "__main__":
-    cap = cv2.VideoCapture(0)
+
+def capture_image(cam_index):
+    cap = cv2.VideoCapture(cam_index)
     ret, frame = cap.read()
     cap.release()
+    return ret, frame
 
-    if not ret:
-        print("Couldn't read frame from webcam.")
+def merge_images(left_img, right_img):
+    return np.concatenate((left_img, right_img), axis=1)
+
+
+if __name__ == "__main__":
+    # Capture images from two webcams
+    ret_left, frame_left = capture_image(0)
+    ret_right, frame_right = capture_image(1)
+
+    if not ret_left or not ret_right:
+        print("Couldn't read frame from one or both webcams.")
         exit()
 
+    # Merge the captured images
+    merged_image = merge_images(frame_left, frame_right)
+
+    # Save the merged image
     project_directory = os.path.dirname(os.path.abspath(__file__))
-
-    image_filename = 'ballon_webcam.jpg'
+    image_filename = 'merged_webcam.jpg'
     image_path = os.path.join(project_directory, image_filename)
-    cv2.imwrite(image_path, frame)
-    print(f"Image saved as {image_path}")
+    cv2.imwrite(image_path, merged_image)
+    print(f"Merged image saved as {image_path}")
 
+    # Preprocess the image
     output_filename = 'preprocessed_' + image_filename
     output_path = os.path.join(project_directory, output_filename)
-
     preprocessor = ImagePreprocessor(image_path, output_path)
     preprocessor.preprocess()
 
+    # Perform OCR on the preprocessed image
     config = '--oem 1 --psm 6 -c tessedit_create_hocr=1 -c tessedit_write_images=True'
     ocr = TesseractOCR(config)
     text = ocr.image_to_string(output_path)

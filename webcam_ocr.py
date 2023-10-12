329d85924f5e8203b648cd6baf1aadeacc11bda3
Дарья <kud4v@yandex.ru>
2023-10-12 17:40:53 +0300

    c2 webcam

--- webcam_ocr.py
+++ webcam_ocr.py
@@ -73,36 +73,46 @@ def merge_images(left_img, right_img):
     return np.concatenate((left_img, right_img), axis=1)
 
 
+def crop_to_square(image):
+    height, width, _ = image.shape
+    size = min(height, width)
+    x = (width - size) // 2
+    y = (height - size) // 2
+    return image[y:y+size, x:x+size]
+
+def merge_cropped_images(left_img, right_img):
+    cropped_left = crop_to_square(left_img)
+    cropped_right = crop_to_square(right_img)
+
+    return np.concatenate((cropped_left, cropped_right), axis=1)
+
+
 if __name__ == "__main__":
-    # Capture images from two webcams
     ret_left, frame_left = capture_image(0)
-    ret_right, frame_right = capture_image(1)
+    ret_right, frame_right = capture_image(0)
 
     if not ret_left or not ret_right:
         print("Couldn't read frame from one or both webcams.")
         exit()
 
-    # Merge the captured images
+
     merged_image = merge_images(frame_left, frame_right)
 
-    # Save the merged image
     project_directory = os.path.dirname(os.path.abspath(__file__))
     image_filename = 'merged_webcam.jpg'
     image_path = os.path.join(project_directory, image_filename)
     cv2.imwrite(image_path, merged_image)
     print(f"Merged image saved as {image_path}")
 
-    # Preprocess the image
-    output_filename = 'preprocessed_' + image_filename
-    output_path = os.path.join(project_directory, output_filename)
-    preprocessor = ImagePreprocessor(image_path, output_path)
-    preprocessor.preprocess()
+    #output_filename = 'preprocessed_' + image_filename
+    #output_path = os.path.join(project_directory, output_filename)
+    #preprocessor = ImagePreprocessor(image_path, output_path)
+    #preprocessor.preprocess()
 
-    # Perform OCR on the preprocessed image
-    config = '--oem 1 --psm 6 -c tessedit_create_hocr=1 -c tessedit_write_images=True'
-    ocr = TesseractOCR(config)
-    text = ocr.image_to_string(output_path)
-    print(text)
+    #config = '--oem 1 --psm 6 -c tessedit_create_hocr=1 -c tessedit_write_images=True'
+    #ocr = TesseractOCR(config)
+    #text = ocr.image_to_string(output_path)
+    #print(text)
 
     #data = pytesseract.image_to_data(output_path, config=config)
     #data = ocr.image_to_data(output_path)

b40fe1c062f07974d5156412174e4007a3c1ff23
Дарья <kud4v@yandex.ru>
2023-10-22 18:18:25 +0300

    add app

--- glasCylindersApp.py
+++ glasCylindersApp.py
@@ -5,6 +5,8 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QSlider, QVBoxLay
 from PyQt5.uic import loadUi
 from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtCore import QTimer, Qt
+import pytesseract
+
 
 
 class MainWindow(QMainWindow):
@@ -12,10 +14,11 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__()
         loadUi("glasCylinders.ui", self)
 
-        # Инициализация слайдеров и их начальные значения
+        pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
         self.slider_contrast.setValue(50)
-        self.slider_binary.setValue(50)  # Начальное значение для порога бинаризации
-        self.slider_noise.setValue(0)  # Начальное значение для сглаживания шума
+        self.slider_binary.setValue(50)
+        self.slider_noise.setValue(0)
 
         self.video_feed()
 
@@ -23,7 +26,14 @@ class MainWindow(QMainWindow):
         self.capture = cv2.VideoCapture(0)
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.update_frame)
+
+
+
         self.timer.start(30)
+
+    def calculate_accuracy(self, recognized_text, expected_text):
+        matched_chars = sum(1 for a, b in zip(recognized_text, expected_text) if a == b)
+        return matched_chars / max(len(recognized_text), len(expected_text))
 
     def update_frame(self):
         ret, image = self.capture.read()
@@ -33,6 +43,18 @@ class MainWindow(QMainWindow):
             image = self.adjust_contrast(image, self.slider_contrast.value())
             binary = self.apply_binary_threshold(gray, self.slider_binary.value())
             noise_reduced = self.reduce_noise(binary, self.slider_noise.value())
+
+            # Распознавание текста с помощью pytesseract
+            recognized_text = pytesseract.image_to_string(noise_reduced, lang='rus')
+
+            # Вычисление точности
+            expected_text = "Ваш ожидаемый текст"  # Замените на ваш ожидаемый текст
+            accuracy = self.calculate_accuracy(recognized_text, expected_text)
+            accuracy_percentage = round(accuracy * 100, 2)
+
+            # Вывод распознанного текста и точности в виджеты QLabel
+            self.text_result.setText(recognized_text)
+            self.text_accuracy.setText(f"Accuracy: {accuracy_percentage}%")
 
             height, width = noise_reduced.shape
             bytes_per_line = width
@@ -47,13 +69,11 @@ class MainWindow(QMainWindow):
         return binary
 
     def reduce_noise(self, gray_image, noise_value):
-        # Пример использования GaussianBlur для сглаживания
-        return cv2.GaussianBlur(gray_image, (noise_value | 1, noise_value | 1),
-                                0)  # noise_value|1 гарантирует нечетное значение
+        return cv2.GaussianBlur(gray_image, (noise_value | 1, noise_value | 1),0)
 
     def adjust_contrast(self, image, contrast_value):
         alpha = contrast_value / 100.0
-        beta = 0  # не меняем яркость
+        beta = 0
         return cv2.convertScaleAbs(image, alpha=alpha, beta=beta)
 
 
